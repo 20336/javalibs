ce0f1eb3421e84f85026d7c633c1d29ba1f73d69
Raven Dell <20336@my4county.net>
2021-09-10 09:56:15 -0400

    finished the story

--- src/javalibs/JavaLibs.java
+++ src/javalibs/JavaLibs.java
@@ -27,85 +27,69 @@ public class JavaLibs {
         String nameOfPerson;
         String destination;
         String friendName;
-        String cakeFlavor;
+        String cakeDescriptor;
+        String birthdayActivities;
+        String emotion;
         
         int ageNumber;
         int yearsAgo;
         int total;
         
+        double personWeight;
         double cakeWeight;
-        double wantedCakeWeight;
-        double cakeDifference;
+        double weightDifference;
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("A person sits alone at their desk. What is this "
-                + "person's name? ");
+        System.out.print("Insert a name: ");
             nameOfPerson = sc.nextLine();
-        
-        System.out.println("Today is " 
-                + nameOfPerson + "'s birthday, and they are completely alone "
-                        + "apart from the voice on their screen.");
-        
-        System.out.print("Since it is " + nameOfPerson + "'s birthday, how "
-                + "old are they turning? ");
-            ageNumber = sc.nextInt();
-            
-        System.out.println("Unbeknownst to you, you are " + nameOfPerson 
-                + " and you are alone on your birthday. But at least you have "
-                        + "me, the voice on your screen.");
-            
-        
-        
-        System.out.print("Anyways, congratulations on turning " + ageNumber 
-                + "! I'm proud of you, I didn't think you'd make it this far, "
-                + "especially after your trip to ");
+        System.out.print("Insert a place: ");
             destination = sc.next();
+        System.out.print("Insert a name: ");
+            friendName = sc.next();
+        System.out.print("Insert an adjective: ");
+            cakeDescriptor = sc.next();
+        System.out.print("Insert a word that ends in \"ing\": ");
+            birthdayActivities = sc.next();
+        System.out.print("Insert an emotion: ");
+            emotion = sc.next();
         
-        System.out.println("After the trip you were never the same, you started"
-                + " being more risky in your actions.");
-        
-        System.out.print(nameOfPerson + ", how many years was it since the trip"
-                + " to " + destination + "? ");
+        System.out.print("Insert a number(int): ");
+            ageNumber = sc.nextInt();
+        System.out.print("Insert a number(int): ");
             yearsAgo = sc.nextInt();
             
-        total = ageNumber - yearsAgo;
-        System.out.println("WOW! " + yearsAgo+ " years ago. You were " + total 
-                + ", that feel hard to beli- ");
-        
-        System.out.print("Did you just hear that? Your door creaked, it was "
-                + "one of your friends. What was their name? ");
-            friendName = sc.next();
-            
-        System.out.println(friendName + " already left. Awe, but "
-                + "look what " + friendName + " left you, a birthday "
-                + "cake, how sweet. Go pick it up and tell me how much it "
-                + "weighs.");
-        
-        System.out.print("How many pounds do you think it weighs? ");
+        System.out.print("Insert a number(double): ");
             cakeWeight = sc.nextDouble();
+        System.out.print("Insert a number(double): ");
+            personWeight = sc.nextDouble();
             
-        System.out.print(nameOfPerson + ", why would " + friendName 
-                + " make you a cake that only weighs " + cakeWeight + "? "
-                + "That's horrible, it's your birthday, how much do "
-                + "you want your birthcake to actually weigh? ");
-            wantedCakeWeight = sc.nextDouble();
+        total = ageNumber - yearsAgo;
+        weightDifference = personWeight - cakeWeight;
         
-        cakeDifference = cakeWeight - wantedCakeWeight;
-        System.out.println("That's a whole " + cakeDifference + "pound "
-                + "difference! Just awful, I can not believe " 
-                + friendName + ".");
         
-        System.out.print("Is it even the right flavor, what's your favorite "
-                + "cake flavor?");
-            cakeFlavor = sc.next();
-            
-        System.out.println("Man, it's your birthday and you can not catch a "
-                + "break, first your alone besides me, the voice on your screen"
-                + ", second your cake isn't even the right size, and third "
-                + "it's not even the right flavor. " + nameOfPerson 
-                + ", I hope your birthday gets better."); 
-            
+        
+        System.out.println("There name is " + nameOfPerson + " Today is " 
+                + nameOfPerson + "'s birthday. ");
+        
+        System.out.println(nameOfPerson + " is turning "  + ageNumber + ". For"
+                + " their birthday, they are going to " + destination + ".");
+        
+        System.out.println("It has  been " + yearsAgo + " since they had gone "
+                + "there last, they were only " + total + "!");
+        
+        System.out.println(" For their birthday their friend, " + friendName 
+                + ", baked them a " + cakeDescriptor + " cake. ");
+        
+        System.out.println(nameOfPerson + " weights " + personWeight + "kg and"
+                + " their cake weights " + cakeWeight + "kg, the difference "
+                        + "between them is " + weightDifference+ "kg.");
+        
+        System.out.println("For "+ nameOfPerson + "'s birthday, they are going"
+                + " to be " + birthdayActivities + " at " + destination + ".");
+        
+        System.out.println("They are so " +emotion+ " to be at " + destination 
+                + " because they have waited so long to come.");
             
     }
     
